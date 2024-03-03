e354a419b55f9ddd5f34aeb98f9e982d73add22d
jakemerlin <jakemerlin123@gmail.com>
2024-03-03 23:30:02 +0800

    code added

--- js.js
+++ js.js
@@ -1,56 +1,7 @@
-class Students{
+const book_form = document.querySelector('#book-form');
 
-    constructor(firstname, lastname, middlename, school, block){
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.middlename = middlename;
-        this.school = school;
-        this.block = block;
-    }
 
-    static addStudent(student){
-       const studentsList = document.querySelector('#student-list');
-       const row = document.createElement('tr');
-
-        row.innerHTML =`
-        <td>${student.firstname}</td>
-        <td>${student.lastname}</td>
-        <td>${student.middlename}</td>
-        <td>${student.school}</td>
-        <td>${student.block}</td>
-        <td><a href="#" class="btn btn-danger btn-sm remove">Remove</a></td>
-        `;
-
-        studentsList.appendChild(row);
-    }
-
-    static ShowAlert(message, className){
-       const div = document.createElement('div');
-        div.className = `alert alert-${className}`;
-        div.appendChild(document.createTextNode(message));
-        const container = document.querySelector('.container');
-        const form = document.querySelector('#book-form');
-        container.insertBefore(div,form);
-
-        setTimeout((e) => document.querySelector('.alert').remove(), 3000);
-    }
-
-    static clearFields(){
-        const firstname = document.querySelector('#firstname').value = "";
-        const lastname = document.querySelector('#lastname').value = "";
-        const middlename = document.querySelector('#middlename').value = "";
-        const school = document.querySelector('#School').value = "";
-        const block = document.querySelector('#Block').value = "";
-    }
-
-    static removeStudent(e){
-        if(e.classList.contains('remove')){
-            e.parentElement.parentElement.remove();
-        }
-    }
-}
-
-document.querySelector('#book-form').addEventListener('submit', (e) => {
+book_form.addEventListener('submit', (e)=>{
     e.preventDefault();
 
     const firstname = document.querySelector('#firstname').value;
@@ -60,19 +11,48 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const block = document.querySelector('#Block').value;
 
     if(firstname === "" || lastname === "" || middlename === "" || school === "" || block ===""){
-        Students.ShowAlert("Plese Fill the Blank", "danger");
-    }else{
-        Students.ShowAlert("Student Added", "success");
-        const student = new Students(firstname, lastname, middlename,school, block);
-        Students.addStudent(student);
-        Students.clearFields();
-    }
-});
+        const alert = document.querySelector('#alert');  
+        alert.classList.remove('d-none');     
+            alert.classList.add('alert-danger');
+            alert.innerText = "Plese Fill the Blank";
+            setTimeout((e) => document.querySelector('#alert').remove(), 3000);
+             }else{
+            const alert = document.querySelector('#alert');  
+            alert.classList.remove('d-none');
+            alert.classList.remove('alert-danger');       
+            alert.classList.add('alert-success');
+            alert.innerText = "Student Added", "success";
 
-document.querySelector('#student-list').addEventListener('click',(e) => {
-    Students.removeStudent(e.target);
-});
+            const studentsList = document.querySelector('#student-list');
+                    const row = document.createElement('tr');
+                     row.innerHTML =`
+                     <td>${firstname}</td>
+                     <td>${lastname}</td>
+                     <td>${middlename }</td>
+                     <td>${school}</td>
+                     <td>${block}</td>
+                     <td><a href="#" class="btn btn-danger btn-sm remove">Remove</a></td>
+                     `;
+            
+                 studentsList.appendChild(row);
 
+                 document.querySelector('#firstname').value = "";
+                 document.querySelector('#lastname').value = "";
+                 document.querySelector('#middlename').value = "";
+                 document.querySelector('#School').value = "";
+                 document.querySelector('#Block').value = "";
+
+                 row.querySelector('.remove').addEventListener('click', () => {
+                    row.remove();
+                });
+
+                 setTimeout(() => {
+                    alert.classList.add('d-none');
+                    alert.innerText = "";
+                }, 3000);
+             }
+
+});
 
 
 
